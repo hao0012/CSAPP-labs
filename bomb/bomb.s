73dac41dbb4997a34bc839ac9638c6f01101b618
junhao <709704094@qq.com>
2023-01-22 16:24:43 +0800

    finish secret_phase

--- bomb/bomb.s
+++ bomb/bomb.s
@@ -687,13 +687,16 @@ Disassembly of section .text:
   401204:	48 83 ec 08          	sub    $0x8,%rsp
   401208:	48 85 ff             	test   %rdi,%rdi
   40120b:	74 2b                	je     401238 <fun7+0x34>
+
   40120d:	8b 17                	mov    (%rdi),%edx
   40120f:	39 f2                	cmp    %esi,%edx
   401211:	7e 0d                	jle    401220 <fun7+0x1c>
+  # condition 2
   401213:	48 8b 7f 08          	mov    0x8(%rdi),%rdi
   401217:	e8 e8 ff ff ff       	call   401204 <fun7>
   40121c:	01 c0                	add    %eax,%eax
   40121e:	eb 1d                	jmp    40123d <fun7+0x39>
+  # condition 1
   401220:	b8 00 00 00 00       	mov    $0x0,%eax
   401225:	39 f2                	cmp    %esi,%edx
   401227:	74 14                	je     40123d <fun7+0x39>
@@ -701,6 +704,7 @@ Disassembly of section .text:
   40122d:	e8 d2 ff ff ff       	call   401204 <fun7>
   401232:	8d 44 00 01          	lea    0x1(%rax,%rax,1),%eax
   401236:	eb 05                	jmp    40123d <fun7+0x39>
+  
   401238:	b8 ff ff ff ff       	mov    $0xffffffff,%eax
   40123d:	48 83 c4 08          	add    $0x8,%rsp
   401241:	c3                   	ret    
@@ -713,19 +717,24 @@ Disassembly of section .text:
   401252:	48 89 c7             	mov    %rax,%rdi
   401255:	e8 76 f9 ff ff       	call   400bd0 <strtol@plt>
   40125a:	48 89 c3             	mov    %rax,%rbx
+  # eax--, if eax > 0x3e8(1000) (input > 1001), bomb!!!
   40125d:	8d 40 ff             	lea    -0x1(%rax),%eax
   401260:	3d e8 03 00 00       	cmp    $0x3e8,%eax
   401265:	76 05                	jbe    40126c <secret_phase+0x2a>
   401267:	e8 ce 01 00 00       	call   40143a <explode_bomb>
+  # fun7(36, input_number)
   40126c:	89 de                	mov    %ebx,%esi
   40126e:	bf f0 30 60 00       	mov    $0x6030f0,%edi
   401273:	e8 8c ff ff ff       	call   401204 <fun7>
+  
   401278:	83 f8 02             	cmp    $0x2,%eax
   40127b:	74 05                	je     401282 <secret_phase+0x40>
   40127d:	e8 b8 01 00 00       	call   40143a <explode_bomb>
+  
   401282:	bf 38 24 40 00       	mov    $0x402438,%edi
   401287:	e8 84 f8 ff ff       	call   400b10 <puts@plt>
   40128c:	e8 33 03 00 00       	call   4015c4 <phase_defused>
+  
   401291:	5b                   	pop    %rbx
   401292:	c3                   	ret    
   401293:	90                   	nop
@@ -877,6 +886,7 @@ Disassembly of section .text:
 
 00000000004013f9 <skip>:
   4013f9:	53                   	push   %rbx
+  
   4013fa:	48 63 05 5f 23 20 00 	movslq 0x20235f(%rip),%rax        # 603760 <num_input_strings>
   401401:	48 8d 3c 80          	lea    (%rax,%rax,4),%rdi
   401405:	48 c1 e7 04          	shl    $0x4,%rdi
@@ -1008,34 +1018,45 @@ Disassembly of section .text:
   4015cf:	00 00 
   4015d1:	48 89 44 24 68       	mov    %rax,0x68(%rsp)
   4015d6:	31 c0                	xor    %eax,%eax
+  # don't jump when phase_6 defused
   4015d8:	83 3d 81 21 20 00 06 	cmpl   $0x6,0x202181(%rip)        # 603760 <num_input_strings>
   4015df:	75 5e                	jne    40163f <phase_defused+0x7b>
+  
   4015e1:	4c 8d 44 24 10       	lea    0x10(%rsp),%r8
   4015e6:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx
   4015eb:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx
   4015f0:	be 19 26 40 00       	mov    $0x402619,%esi
   4015f5:	bf 70 38 60 00       	mov    $0x603870,%edi
   4015fa:	e8 f1 f5 ff ff       	call   400bf0 <__isoc99_sscanf@plt>
+  # to defuse secret_phase, in phase_4 we must input an extra string "DrEvil"
   4015ff:	83 f8 03             	cmp    $0x3,%eax
   401602:	75 31                	jne    401635 <phase_defused+0x71>
+  
   401604:	be 22 26 40 00       	mov    $0x402622,%esi
   401609:	48 8d 7c 24 10       	lea    0x10(%rsp),%rdi
   40160e:	e8 25 fd ff ff       	call   401338 <strings_not_equal>
+  
   401613:	85 c0                	test   %eax,%eax
   401615:	75 1e                	jne    401635 <phase_defused+0x71>
+  
   401617:	bf f8 24 40 00       	mov    $0x4024f8,%edi
   40161c:	e8 ef f4 ff ff       	call   400b10 <puts@plt>
+  
   401621:	bf 20 25 40 00       	mov    $0x402520,%edi
   401626:	e8 e5 f4 ff ff       	call   400b10 <puts@plt>
+  
   40162b:	b8 00 00 00 00       	mov    $0x0,%eax
   401630:	e8 0d fc ff ff       	call   401242 <secret_phase>
+  
   401635:	bf 58 25 40 00       	mov    $0x402558,%edi
   40163a:	e8 d1 f4 ff ff       	call   400b10 <puts@plt>
+
   40163f:	48 8b 44 24 68       	mov    0x68(%rsp),%rax
   401644:	64 48 33 04 25 28 00 	xor    %fs:0x28,%rax
   40164b:	00 00 
   40164d:	74 05                	je     401654 <phase_defused+0x90>
   40164f:	e8 dc f4 ff ff       	call   400b30 <__stack_chk_fail@plt>
+  
   401654:	48 83 c4 78          	add    $0x78,%rsp
   401658:	c3                   	ret    
   401659:	90                   	nop
